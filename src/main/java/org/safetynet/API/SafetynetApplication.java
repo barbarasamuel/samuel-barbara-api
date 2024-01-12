05307cfaffe0dcb5f6d830877ecce859e39af041
Barbara Samuel <barbara.samuel@yahoo.fr>
2024-01-12 11:42:08 +0100

    Premier commit du projet

---
+++ src/main/java/org/safetynet/API/SafetynetApplication.java
@@ -0,0 +1,13 @@
+package org.safetynet.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetynetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetynetApplication.class, args);
+	}
+
+}
